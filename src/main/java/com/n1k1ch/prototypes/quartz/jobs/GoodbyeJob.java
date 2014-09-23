9ce33f697c45bebb1e45da68c32dc9d6db0aba00
n1k1ch <nikita.cherevkov@gmail.com>
2014-09-24 02:10:21 +0300

    added loggers

--- src/main/java/com/n1k1ch/prototypes/quartz/jobs/GoodbyeJob.java
+++ src/main/java/com/n1k1ch/prototypes/quartz/jobs/GoodbyeJob.java
@@ -1,17 +1,22 @@
 package com.n1k1ch.prototypes.quartz.jobs;
 
+import org.apache.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 
 import java.util.Locale;
+import java.util.logging.Level;
 
 /**
  * Created by n1k1ch on 24.09.2014.
  */
 public class GoodbyeJob implements Job {
+
+    private static final Logger log = Logger.getLogger(GoodbyeJob.class);
+
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        System.out.println(String.format(Locale.getDefault(), "Goodbye! Next goodbye at %s", context.getNextFireTime()));
+        log.info(String.format(Locale.getDefault(), "Goodbye! Next goodbye at %s", context.getNextFireTime()));
     }
 }
--- src/main/java/com/n1k1ch/prototypes/quartz/jobs/HelloJob.java
+++ src/main/java/com/n1k1ch/prototypes/quartz/jobs/HelloJob.java
@@ -1,5 +1,6 @@
 package com.n1k1ch.prototypes.quartz.jobs;
 
+import org.apache.log4j.Logger;
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
@@ -10,9 +11,10 @@ import java.util.Locale;
  * Created by n1k1ch on 24.09.2014.
  */
 public class HelloJob implements Job {
+    private static final Logger log = Logger.getLogger(HelloJob.class);
 
     @Override
     public void execute(JobExecutionContext context) throws JobExecutionException {
-        System.out.println(String.format(Locale.getDefault(), "Hello. Next hello at %s", context.getNextFireTime()));
+        log.info(String.format(Locale.getDefault(), "Hello. Next hello at %s", context.getNextFireTime()));
     }
 }
